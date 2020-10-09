a6cac4f31177a6962676bc9f5ea39b882d3085d2
Joaquin <xvitkleo@gmail.com>
2020-10-08 19:06:50 -0500

    terminado

--- jugar/main.js
+++ jugar/main.js
@@ -1,13 +1,7 @@
-var life = 3;
+var lifes = 3;
+var timeToMove = 3;
+var nivel = 1;
 
-function actualizarVida() {
-    var cont=3;
-    life--;
-    document.querySelectorAll(".heart").forEach(heart => {
-        cont--;
-        if(cont==life) heart.style.color = "#a09b98";
-    })
-}
 
 function mover(accion, mapaActual, mapaMinas){
     acciones = ["up", "down", "left", "right"]
@@ -148,14 +142,14 @@ function actualizarMatriz(matriz) {
             if(y==='+') {
                 document.querySelector(`.c${cont}`).innerHTML = '<img src="https://opengameart.org/sites/default/files/robot-idle.gif">';                
             }
-            if(y==='0' || y=='#') {
+            if(y==='0') {
                 document.querySelector(`.c${cont}`).textContent = "";
                 document.querySelector(`.c${cont}`).parentElement.style.backgroundColor = "#70B7E0";
             }
             if(y=='$') {
                 document.querySelector(`.c${cont}`).innerHTML = '<img src="https://esraa-alaarag.github.io/Minesweeper/images/bomb.png">';                
             }
-            if(y=='@') {
+            if(y=='@' || y=='#') {
                 document.querySelector(`.c${cont}`).innerHTML = '<img src="https://cdnb.artstation.com/p/assets/images/images/011/517/245/original/isa-deu-flag.gif?1529988984">';                    
             }
             cont++;
@@ -163,85 +157,123 @@ function actualizarMatriz(matriz) {
     });
 }
 
-//ejemplo
+
+
+function reiniciar() {    
+    mapaMinas = generarMapa();
+    mapaActual = mapaInicial;
+    mapaPrevio = mapaActual;
+    actualizarMatriz(obtenerMatrixDeMapa(mapaActual));
+    lifes = 3;
+    document.querySelector(".arrows").style.visibility = "visible"; 
+    document.querySelectorAll(".heart").forEach(heart => heart.style.color = "#70b7e0");
+    document.querySelector('.nivel').textContent = ++nivel;
+}
+
 mapaInicial = `
 0000#
 00000
 00000
 00000
 +0000
-`
-mapaMinas = `
-0000#
-0$$$0
-0$000
-0$000
-+$000
-`
+`;
 
-mapaActual = mapaInicial
-console.log(mapaActual)
-mapaPrevio = mapaActual
+mapaMinas = generarMapa();
+mapaActual = mapaInicial;
+mapaPrevio = mapaActual;
 actualizarMatriz(obtenerMatrixDeMapa(mapaActual));
+console.log(mapaActual);
 
 document.querySelectorAll(".arrow").forEach(arrow => {
     arrow.firstChild.addEventListener("click", function() {
-        if(life>0) {
-            var clase = arrow.firstChild.className;
-            if(clase.includes("up")) mapaActual = mover("up", mapaActual, mapaMinas);
-            else if(clase.includes("down")) mapaActual = mover("down", mapaActual, mapaMinas);
-            else if(clase.includes("right")) mapaActual = mover("right", mapaActual, mapaMinas);
-            else if(clase.includes("left")) mapaActual = mover("left", mapaActual, mapaMinas);
-            actualizarMatriz(obtenerMatrixDeMapa(mapaActual));        
-            var resultado = obtenerResultado(mapaActual, mapaPrevio);
-            document.querySelector('.resultado').textContent = resultado.toUpperCase();
-            if(resultado == "fin") {
-                document.querySelector(".ganar").style.display = "grid";   
-                document.querySelector('.resultado').textContent = "FELICIDADES"; 
-                life=0;        
-            }
-            else if(resultado == "mina") {
-                actualizarVida();   
-                verificarEstado(); 
-            }    
-            mapaPrevio = mapaActual;  
-        }              
+        if(lifes>0) {
+            document.querySelector(".arrows").style.visibility = 'hidden';
+            var timer = setInterval(function(){
+                document.querySelector(".resultado").textContent = timeToMove;
+                if(timeToMove<=0) {
+                    if(lifes>0) {
+                        var clase = arrow.firstChild.className;
+                        if(clase.includes("up")) mapaActual = mover("up", mapaActual, mapaMinas);
+                        else if(clase.includes("down")) mapaActual = mover("down", mapaActual, mapaMinas);
+                        else if(clase.includes("right")) mapaActual = mover("right", mapaActual, mapaMinas);
+                        else if(clase.includes("left")) mapaActual = mover("left", mapaActual, mapaMinas);
+                        actualizarMatriz(obtenerMatrixDeMapa(mapaActual));        
+                        var resultado = obtenerResultado(mapaActual, mapaPrevio);
+                        document.querySelector('.resultado').textContent = resultado.toUpperCase();
+                        if(resultado == "fin") {
+                            document.querySelector(".ganar").style.display = "grid";   
+                            document.querySelector('.resultado').textContent = "FELICIDADES"; 
+                            lifes=0;      
+                        }
+                        else if(resultado == "mina") {
+                            document.querySelector('.resultado').textContent = "EXPLOSION";
+                            actualizarVida();   
+                            verificarEstado(); 
+                        }    
+                        else if(resultado == "sin mina") {
+                            document.querySelector('.resultado').textContent = "ALIVIO";
+                        }    
+                        mapaPrevio = mapaActual;  
+                        timeToMove = 4;
+                        clearInterval(timer); 
+                        document.querySelector(".arrows").style.visibility = 'visible';
+                    }    
+                } 
+                timeToMove -= 1;
+            }, 1000);
+        }    
+                      
     })
 })
 
 document.addEventListener("keydown", function(e) {  
-    if(life>0){
-        switch (e.key) {
-            case "ArrowUp":
-                mapaActual = mover("up", mapaActual, mapaMinas) 
-              break;
-            case "ArrowDown":
-                mapaActual = mover("down", mapaActual, mapaMinas)
-                break;
-            case "ArrowRight":
-                mapaActual = mover("right", mapaActual, mapaMinas)
-                break;
-            case "ArrowLeft":
-                mapaActual = mover("left", mapaActual, mapaMinas)
-                break;
-            default:         
-              break;
-        } 
-        actualizarMatriz(obtenerMatrixDeMapa(mapaActual));
-        var resultado = obtenerResultado(mapaActual, mapaPrevio);
-        document.querySelector('.resultado').textContent = resultado.toUpperCase();
-        if(resultado == "fin") {
-            document.querySelector(".ganar").style.display = "grid";    
-            document.querySelector('.resultado').textContent = "FELICIDADES";
-            life=0;        
-        }
-        else if(resultado == "mina") {
-            actualizarVida();   
-            verificarEstado(); 
-        }        
-        mapaPrevio = mapaActual;   
-    }
-     
+    if(document.querySelector(".arrows").style.visibility!="hidden" && lifes>0) {     
+        console.log(mapaActual);
+        document.querySelector(".arrows").style.visibility = 'hidden'
+        var timer = setInterval(function(){
+            document.querySelector(".resultado").textContent = timeToMove;
+            if(timeToMove<=0) {                
+                    switch (e.key) {
+                        case "ArrowUp":
+                            mapaActual = mover("up", mapaActual, mapaMinas) 
+                            break;
+                        case "ArrowDown":
+                            mapaActual = mover("down", mapaActual, mapaMinas)
+                            break;
+                        case "ArrowRight":
+                            mapaActual = mover("right", mapaActual, mapaMinas)
+                            break;
+                        case "ArrowLeft":
+                            mapaActual = mover("left", mapaActual, mapaMinas)
+                            break;
+                        default:         
+                        break;
+                    } 
+                    actualizarMatriz(obtenerMatrixDeMapa(mapaActual));
+                    var resultado = obtenerResultado(mapaActual, mapaPrevio);
+                    document.querySelector('.resultado').textContent = resultado.toUpperCase();
+                    if(resultado == "fin") {
+                        document.querySelector(".ganar").style.display = "grid";    
+                        document.querySelector('.resultado').textContent = "FELICIDADES";
+                        lifes=0;                               
+                    }
+                    else if(resultado == "mina") {
+                        document.querySelector('.resultado').textContent = "EXPLOSION";
+                        actualizarVida();   
+                        verificarEstado(); 
+                    }        
+                    else if(resultado == "sin mina") {
+                        document.querySelector('.resultado').textContent = "ALIVIO";
+                    }
+                    mapaPrevio = mapaActual;  
+                    timeToMove = 4;
+                    document.querySelector(".arrows").style.visibility = 'visible';
+                    if(lifes<=0) document.querySelector(".arrows").style.visibility = "hidden"; 
+                    clearInterval(timer);                    
+            } 
+            timeToMove -= 1;
+        }, 0);  
+    }  
 })
 
 document.querySelector(".regresar").addEventListener("click", function() {
@@ -249,22 +281,56 @@ document.querySelector(".regresar").addEventListener("click", function() {
 })
 
 document.querySelectorAll(".boton_aceptar").forEach(boton => {
-    boton.addEventListener('click', function(){
-        document.querySelectorAll(".modal").forEach(modal => modal.style.display = "none");
+    boton.addEventListener('click', function(e){
+        document.querySelectorAll(".modal").forEach(modal => modal.style.display = "none");        
+        if(e.target.className.includes("boton_ganar")) reiniciar();
     })
 })
 
 function verificarEstado() {
-    if(life<0) life=0;
-    if(life==0) {    
+    if(lifes<0) lifes=0;
+    if(lifes==0) {    
         document.querySelector('.resultado').textContent = "ROBOT DESTRUIDO";
         document.querySelector(".perder").style.display = "grid";
+        document.querySelector(".arrows").style.visibility = "hidden"; 
     }
 }
 
+function actualizarVida() {
+    var cont=3;
+    lifes--;
+    document.querySelectorAll(".heart").forEach(heart => {
+        cont--;
+        if(cont==lifes) heart.style.color = "#a09b98";
+    })
+}
+
+function generarMapa() {
+    var posMinas = [];
+	var cont = 0;
+	var mapaB = "0000000000000000000000000".split('');
+    mapaB[20] = '+';
+    mapaB[4] = '#';
+    cont = 0;
+	for(var i=0; i<6; i++) {
+    	rand = Math.floor(Math.random()*25);
+  		while(posMinas.includes(rand) || rand == 20 || rand == 4) rand = Math.floor(Math.random()*(25-1))+1;
+    	posMinas[cont++] = rand;
+        mapaB[rand] = '$';
+    }
+    var mapa = '';
+    for(var i=0; i<mapaB.length; i++) {    	
+    	mapa+=mapaB[i];
+        if((i+1)%5==0) mapa+='\n';
+    }
+    return mapa;
+}
+
+
 document.querySelector(".reiniciar").addEventListener("click", function() {
-    window.location.href = "./Jugar.html";
-}) 
+    window.location.href = './Jugar.html';
+})
+
 
 
 
--- main.js
+++ main.js
@@ -5,3 +5,19 @@ window.onload = function() {
 document.querySelector(".boton_aceptar").addEventListener("click", function() {
     document.querySelector(".modal").style.display = "none";
 })
+
+document.querySelector(".instrucciones").addEventListener("click", function() {
+    document.querySelector(".modal_instrucciones").style.display = "grid";
+})
+
+document.querySelector(".boton_instrucciones_aceptar").addEventListener("click", function() {
+    document.querySelector(".modal_instrucciones").style.display = "none";
+})
+
+document.querySelector(".jugar").addEventListener('mouseover', function(e) {
+    e.target.textContent = "Nuevo juego";
+})
+
+document.querySelector(".jugar").addEventListener('mouseout', function(e) {
+    e.target.textContent = "Jugar";
+})
